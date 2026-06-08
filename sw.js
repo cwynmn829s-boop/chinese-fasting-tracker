0c751e4103967dd8e61732c6f51a356674fe50d9
cwynmn829s-boop <cwynmn829s@privaterelay.appleid.com>
2026-06-08 12:52:06 +0100

    Deploy v500 Luxury Emerald Dark Theme with SF-style SVG icons and functional sub-interfaces

--- sw.js
+++ sw.js
@@ -1,31 +1,25 @@
-const CACHE_NAME = 'emerald-fasting-v400';
+const CACHE_NAME = 'fasting-v500';
 const ASSETS = [
-    'index.html',
-    'manifest.json',
-    'https://img.icons8.com/glassmorphism/512/emerald.png'
+    './',
+    './index.html',
+    './manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(k => {
+                if (k !== CACHE_NAME) return caches.delete(k);
+            })
+        ))
     );
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.filter((key) => key !== CACHE_NAME)
-                    .map((key) => caches.delete(key))
-            );
-        })
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
